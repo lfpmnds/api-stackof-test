6a2e8fb49a0f368ccf6b5884facfda3e5b3b324a
Felipe <luizfelipe.m93@gmail.com>
2022-07-27 22:05:56 -0300

    feat: implementação da classe Answer e adição de novos scripts em import.sql

---
+++ src/main/java/com/webradar/stackoverflow/entities/Answer.java
@@ -0,0 +1,93 @@
+package com.webradar.stackoverflow.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_answer")
+public class Answer implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+		
+	private String body;
+	
+	@ManyToOne
+	@JoinColumn(name = "author_id")
+	private User author;
+	
+	@ManyToOne
+	@JoinColumn(name = "question_id")
+	private Question question;
+	
+	public Answer() {
+	}
+
+	public Answer(Long id, String body, User author, Question question) {
+		super();
+		this.id = id;
+		this.body = body;
+		this.author = author;
+		this.question = question;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getBody() {
+		return body;
+	}
+
+	public void setBody(String body) {
+		this.body = body;
+	}
+
+	public User getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(User author) {
+		this.author = author;
+	}
+
+	public Question getQuestion() {
+		return question;
+	}
+
+	public void setQuestion(Question question) {
+		this.question = question;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Answer other = (Answer) obj;
+		return Objects.equals(id, other.id);
+	}
+}
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -2,8 +2,13 @@ INSERT INTO tb_user (username, password) VALUES ('maria_dasilva', '123');
 INSERT INTO tb_user (username, password) VALUES ('joao@email.com', '456');
 INSERT INTO tb_user (username, password) VALUES ('carlosJose2', '789');
 
-
 INSERT INTO tb_question (body, author_id) VALUES ('Não estou conseguindo conectar o banco H2 ao meu projeto. Alguém poderia me ajudar?', 3);
 INSERT INTO tb_question (body, author_id) VALUES ('Como remover a senha do Spring Security que é gerada assim que rodo a minha aplicação?', 1);
 INSERT INTO tb_question (body, author_id) VALUES ('Como gerar um script simples para povoar meu banco de dados SQL?', 2);
 INSERT INTO tb_question (body, author_id) VALUES ('Como dar um findBy usando chave estrangeira?', 2);
+
+INSERT INTO tb_answer (body, author_id, question_id) VALUES ('Você pode tentar verificar o arquivo application.properties', 2, 1);
+INSERT INTO tb_answer (body, author_id, question_id) VALUES ('Olhe a documentação do Spring Security', 3, 2);
+INSERT INTO tb_answer (body, author_id, question_id) VALUES ('Crie um arquivo import.sql na pasta resources com os dados que gostaria de inserir', 3, 3);
+INSERT INTO tb_answer (body, author_id, question_id) VALUES ('Especifique se a dúvida é sobre como escrever o script ou se há algum lugar que faça isso para você', 1, 3);
+INSERT INTO tb_answer (body, author_id, question_id) VALUES ('Dê uma olhada na annotation @Query do Spring JPA, acho que pode ajudar...', 1, 4);
