8b28ede5737d043a9c899fdccf2322c4a70b0687
Luiz Felipe <luizfelipe.m93@gmail.com>
2022-07-28 08:16:49 -0300

    feat: implementação de AnswerService

---
+++ src/main/java/com/webradar/stackoverflow/services/AnswerService.java
@@ -0,0 +1,20 @@
+package com.webradar.stackoverflow.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.webradar.stackoverflow.entities.Answer;
+import com.webradar.stackoverflow.repositories.AnswerRepository;
+
+@Service
+public class AnswerService {
+
+	@Autowired
+	private AnswerRepository repository;
+	
+	public List<Answer> findAll() {
+		return repository.findAll();
+	}	
+}
