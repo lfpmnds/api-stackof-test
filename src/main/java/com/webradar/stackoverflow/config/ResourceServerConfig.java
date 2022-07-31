e83e7d8de7f9d91f3b32f37b88405a0babeec006
Felipe <luizfelipe.m93@gmail.com>
2022-07-30 23:34:40 -0300

    refactor: alterando método configure em ResourceServerConfig

--- src/main/java/com/webradar/stackoverflow/config/ResourceServerConfig.java
+++ src/main/java/com/webradar/stackoverflow/config/ResourceServerConfig.java
@@ -4,7 +4,6 @@ import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
@@ -21,11 +20,8 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 	@Autowired
 	private JwtTokenStore tokenStore;
 	
-	private static final String[] LOGIN_OR_NEW_OR_VIEWER = { "/oauth/token" , "/users/new", "/h2-console/**" };
-	
-	private static final String[] PUBLIC = { "/questions", "/answers" };
-	
-	private static final String[] USER = { "/questions/**", "/answers/**", "/questions/{id}/**", "/answers/{id}/**" };
+	private static final String[] PUBLIC = { "/oauth/token" , "/users/new", "/h2-console/**",
+			"/questions", "/answers" };
 	
 	@Override
 	public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
@@ -41,10 +37,7 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 		}
 		
 		http.authorizeRequests()
-		.antMatchers(LOGIN_OR_NEW_OR_VIEWER).permitAll()
-		.antMatchers(HttpMethod.GET, PUBLIC).permitAll()
-		.antMatchers(HttpMethod.POST, USER).hasAuthority("USER")
-		.antMatchers(HttpMethod.PUT, USER).hasAuthority("USER")
+		.antMatchers(PUBLIC).permitAll()
 		.anyRequest().authenticated();
 	}	
 }
