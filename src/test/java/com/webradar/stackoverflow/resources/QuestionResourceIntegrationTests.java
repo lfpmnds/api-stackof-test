24559eaad4e891ef1f3ee7d1c4232f5cce76e241
Felipe <luizfelipe.m93@gmail.com>
2022-08-01 11:43:11 -0300

    feat: novo teste de integração em QuestionResource

--- src/test/java/com/webradar/stackoverflow/resources/QuestionResourceIntegrationTests.java
+++ src/test/java/com/webradar/stackoverflow/resources/QuestionResourceIntegrationTests.java
@@ -2,6 +2,7 @@ package com.webradar.stackoverflow.resources;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -22,14 +23,10 @@ import org.springframework.util.MultiValueMap;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.webradar.stackoverflow.entities.Question;
 import com.webradar.stackoverflow.entities.User;
-import com.webradar.stackoverflow.services.QuestionService;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 public class QuestionResourceIntegrationTests {
-
-	@Autowired
-	private QuestionService questionService;
 
 	@Autowired
 	private ObjectMapper objectMapper;
@@ -44,36 +41,59 @@ public class QuestionResourceIntegrationTests {
 	private String clientSecret;
 
 	private String existingUsername;
-	private String nonExistingUsername;
 	private String password;
+	
+	private Long existingIdQuestionFromExistingUsername;
 
 	@BeforeEach
 	void setUp() throws Exception {
 		existingUsername = "maria@gmail.com";
-		nonExistingUsername = "felipe@gmail.com";
 		password = "123456";
+		existingIdQuestionFromExistingUsername = 2L;
 	}
 
 	@Test
-	public void insertShouldReturnNewQuestionWhenUserIsAuthenticated() throws Exception {		
+	public void insertShouldReturnNewQuestionWhenUserIsAuthenticated() throws Exception {
 
 		String accessToken = obtainAccessToken(existingUsername, password);
-		
+
 		User user = new User(null, existingUsername, password);
-		
+
 		Question question = new Question(null, "Será que o teste vai passar?", user);
 
 		String jsonBody = objectMapper.writeValueAsString(question);
 
-		ResultActions result = mockMvc.perform(post("/questions/new")
+		ResultActions result = mockMvc
+				.perform(post("/questions/new")
 				.header("Authorization", "Bearer " + accessToken)
-				.content(jsonBody).contentType(MediaType.APPLICATION_JSON)
+				.content(jsonBody)
+				.contentType(MediaType.APPLICATION_JSON)
 				.accept(MediaType.APPLICATION_JSON));
 
 		result.andExpect(status().isCreated());
 		result.andExpect(jsonPath("$.id").exists());
 	}
-	
+
+	@Test
+	public void updateShouldReturnUpdatedQuestionWhenQuestionBelongsToUser() throws Exception {
+		String accessToken = obtainAccessToken(existingUsername, password);
+		User user = new User(null, existingUsername, password);
+
+		Question question = new Question(null, "Será que o teste vai passar?", user);
+
+		String jsonBody = objectMapper.writeValueAsString(question);
+
+		ResultActions result = mockMvc
+				.perform(put("/questions/{id}/edit", existingIdQuestionFromExistingUsername)
+				.header("Authorization", "Bearer " + accessToken)
+				.content(jsonBody)
+				.contentType(MediaType.APPLICATION_JSON)
+				.accept(MediaType.APPLICATION_JSON));
+		
+		result.andExpect(status().isOk());
+		result.andExpect(jsonPath("$.body").value("Será que o teste vai passar?"));
+	}
+
 	private String obtainAccessToken(String username, String password) throws Exception {
 
 		MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
@@ -83,9 +103,11 @@ public class QuestionResourceIntegrationTests {
 		params.add("password", password);
 
 		ResultActions result = mockMvc
-				.perform(post("/oauth/token").params(params).with(httpBasic(clientId, clientSecret))
+				.perform(post("/oauth/token").params(params)
+						.with(httpBasic(clientId, clientSecret))
 						.accept("application/json;charset=UTF-8"))
-				.andExpect(status().isOk()).andExpect(content().contentType("application/json;charset=UTF-8"));
+				.andExpect(status().isOk()).andExpect(content()
+						.contentType("application/json;charset=UTF-8"));
 
 		String resultString = result.andReturn().getResponse().getContentAsString();
 
