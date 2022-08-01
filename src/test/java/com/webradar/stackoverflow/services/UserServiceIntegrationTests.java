3ba43fb8f98757229bcb5623a32abab6a522f876
Felipe <luizfelipe.m93@gmail.com>
2022-08-01 13:51:08 -0300

    feat: novo teste de integração em UserService

--- src/test/java/com/webradar/stackoverflow/services/UserServiceIntegrationTests.java
+++ src/test/java/com/webradar/stackoverflow/services/UserServiceIntegrationTests.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -15,10 +16,12 @@ public class UserServiceIntegrationTests {
 	private UserService service;
 	
 	private String existingUsername;
+	private String nonExistingUsername;
 	
 	@BeforeEach
 	void setUp() throws Exception {
 		existingUsername = "maria@gmail.com";
+		nonExistingUsername = "felipe@gmail.com";
 	}
 	
 	@Test
@@ -26,5 +29,10 @@ public class UserServiceIntegrationTests {
 		Assertions.assertNotNull(service.loadUserByUsername(existingUsername));
 	}
 	
-	
+	@Test
+	public void loadUserByUsernameShouldThrowUsernameNotFoundExceptionWhenUsernameDoesNotExist() {
+		Assertions.assertThrows(UsernameNotFoundException.class, () -> {
+			service.loadUserByUsername(nonExistingUsername);
+		});
+	}	
 }
